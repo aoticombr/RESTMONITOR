90ca8364ebb1b6b5a6d8ed2965c46df4e5cb3491
krobrelus <paulo@aoti.com.br>
2023-10-12 15:06:22 -0400

    implementado jwt

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const querystring = require('querystring');
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const jwt = require('jsonwebtoken');
 
 // Verifique se a pasta "data" existe e a crie, se necessário
 const dataDir = path.join(__dirname, 'data');
 if (!fs.existsSync(dataDir)) {
   fs.mkdirSync(dataDir);
 }
+
+const secretKey = '123456789';
+
+// ... (seu código anterior)
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
@@ -45,34 +50,83 @@ const server = http.createServer((req, res) => {
         res.end('Erro ao salvar dados da solicitação no arquivo.');
       } else {
         console.log(`Dados da solicitação salvos em ${filename}`);
-        if (req.url === '/token' && req.method === 'POST') {
-          console.log('Recebendo solicitação de token...');
 
+        if (req.url === '/token') {
+          console.log('Recebendo solicitação de token...');
+        
           // Extrair o escopo da solicitação (se enviado)
           const requestedScope = parsedData.scope || 'default_scope';
-
-          // Construir a resposta incluindo o escopo solicitado
+        
+          // Construir o payload do token com as informações necessárias
+          const payload = {
+            user_id: 123, // Substitua isso pelo ID do usuário apropriado
+            scope: requestedScope,
+          };
+        
+          // Chave secreta para assinar o token (substitua por sua chave real)
+        
+          // Gerar o token JWT
+          const token = jwt.sign(payload, secretKey, { expiresIn: '300s' });
+        
+          // Construir a resposta incluindo o token JWT
           const tokenResponse = {
-            access_token: "seu_token_aqui",
+            access_token: token,
             expires_in: 300,
             refresh_expires_in: 0,
-            token_type: "Bearer",
-            "not-before-policy": 0,
-            scope: requestedScope, // Incluindo o escopo na resposta
+            token_type: 'Bearer',
+            'not-before-policy': 0,
+            scope: requestedScope,
           };
-
+        
           res.writeHead(200, { 'Content-Type': 'application/json' });
           res.end(JSON.stringify(tokenResponse));
         } else {
-          res.end('Dados da solicitação recebidos e salvos.');
+          // Aqui você pode adicionar a validação do token JWT
+          const authorizationHeader = req.headers['authorization'];
+
+          // Verificar se o cabeçalho "Authorization" está presente e começa com "Bearer "
+          if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
+            const token = authorizationHeader.slice(7); // Remover "Bearer " do cabeçalho
+
+            // Verificar se o token JWT é válido
+            jwt.verify(token, secretKey, (err, decoded) => {
+              if (err) {
+                res.writeHead(401, { 'Content-Type': 'text/plain' });
+                res.end('Token JWT inválido');
+              } else {
+                // Token JWT válido, você pode acessar os dados decodificados em 'decoded'
+                console.log('Token JWT válido', decoded);
+
+                // Agora você pode lidar com diferentes métodos HTTP
+                if (req.method === 'POST') {
+                  // Lógica para o método POST
+                  // ...
+                  res.writeHead(200, { 'Content-Type': 'text/plain' });
+                  res.end('Requisição POST tratada');
+                } else if (req.method === 'GET') {
+                  // Lógica para o método GET
+                  // ...
+                  res.writeHead(200, { 'Content-Type': 'text/plain' });
+                  res.end('Requisição GET tratada');
+                } else {
+                  // Outros métodos HTTP
+                  res.writeHead(405, { 'Content-Type': 'text/plain' });
+                  res.end('Método HTTP não permitido');
+                }
+              }
+            });
+          } else {
+            res.writeHead(401, { 'Content-Type': 'text/plain' });
+            res.end('Token JWT ausente no cabeçalho "Authorization"');
+          }
         }
       }
     });
   });
-
 });
 
 const port = 3003;
 server.listen(port, () => {
   console.log(`O servidor está em execução em http://localhost:${port}`);
 });
+
